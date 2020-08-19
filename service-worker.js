20813e253d9275b261b23ca132614ca410b3a480
Alvin Mantovani <alvin.ictn@gmail.com>
2020-08-19 22:42:20 +0700

    Complete Premiere League v1.0.2

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,6 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const caching = new Promise((resolve,reject) => {
   if(workbox){
-    console.log(workbox)
     resolve(workbox);
   }else{
     reject('workbox tidak ditemukan');
@@ -12,16 +11,16 @@ const caching = new Promise((resolve,reject) => {
 
 caching.then((workbox) => {
   workbox.precaching.precacheAndRoute([
-    { url: "/index.html", revision: '1' },
-    { url: "/manifest.json", revision: '1' },
-    { url: "/bundle.js", revision: '1' },
-    { url: "/src/img/banner/banner.png", revision: '1' },
-    { url: "/src/img/banner/place.svg", revision: '1' },
-    { url: "/src/img/icon/save.svg", revision: '1' },
-    { url: "/src/img/icon/trash.svg", revision: '1' },
-    { url: "/src/img/logo/icon.png", revision: '1' },
-    { url: "/src/img/logo/logo.png", revision: '1' },
-    { url: "/src/img/logo/splash.png", revision: '1' },
+    { url: "./index.html", revision: '1' },
+    { url: "./manifest.json", revision: '1' },
+    { url: "./bundle.js", revision: '1' },
+    { url: "./src/img/banner/banner.png", revision: '1' },
+    { url: "./src/img/banner/place.svg", revision: '1' },
+    { url: "./src/img/icon/save.svg", revision: '1' },
+    { url: "./src/img/icon/trash.svg", revision: '1' },
+    { url: "./src/img/logo/icon.png", revision: '1' },
+    { url: "./src/img/logo/logo.png", revision: '1' },
+    { url: "./src/img/logo/splash.png", revision: '1' },
   ]);  
 
   workbox.routing.registerRoute(
